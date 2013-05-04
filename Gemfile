1656aa7b0f362ab4f9653f6c32f6acff6ec683c2
Shaoshing <shaoshing@me.com>
2013-05-04 20:15:02 +0800

    Add capistrano.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rake'
+gem 'capistrano'
 
 gem 'bcrypt-ruby', :require => 'bcrypt'
 gem 'slim'
---
+++ config/deploy.rb
@@ -0,0 +1,18 @@
+
+server "root@192.168.1.130", :all
+
+namespace :raspi do
+  desc "Use this task will deploy Projecat to Raspi; don't use [cap deploy]"
+  task :deploy do
+    run [
+      "cd projecat",
+      "git pull",
+      "bundle install",
+      "padrino ar:migrate -e production",
+      "mkdir tmp",
+      "kill `cat tmp/thin.pid`",
+      "thin start -d -e production -p 3000 -a 0.0.0.0 --pid tmp/thin.pid",
+      ].join(" && ")
+  end
+
+end
